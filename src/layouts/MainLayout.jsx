bc3721bd2d71a77cfbfceb3db48b778da0caa7b9
Rakshith H N <hnrakshith4@gmail.com>
2026-02-27 14:39:14 +0530

    Updated docker

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import AIchatbot from '../features/landing/components/AIchatbot';
-
 const MainLayout = () => {
   return (
     <div className="App">
@@ -12,7 +10,6 @@ const MainLayout = () => {
         <Outlet />
       </main>
       <Footer />
-      <AIchatbot />
     </div>
   );
 };
